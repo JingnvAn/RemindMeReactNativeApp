09103e8e30f28ac87d2f5fbd34cf0cbff55df3e1
Jingnu An <ajingnv@gmail.com>
2023-07-26 16:07:43 -0700

    add modal to events page

--- screens/EventsPage.tsx
+++ screens/EventsPage.tsx
@@ -1,47 +1,106 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import {IconButton} from "react-native-paper";
-import {Colors} from "react-native/Libraries/NewAppScreen";
+import React, { useState } from 'react';
+import {Text, View, StyleSheet, Modal, Alert, Pressable, Platform} from 'react-native';
 
 const EventsPage = () => {
-    const iconName = 'pencil-outline';
-    const onPressCreateEvent = () => {
-        // open modal up
-        console.log('Pressed');
-    }
+    const [modalVisible, setModalVisible] = useState(false);   
+
 
     return (
-        <View style={styles.OutMostContainer}>
-            <View style={styles.InnerContainer1}>
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              setModalVisible(!modalVisible);
+            }}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Create A New Reminder 🔔</Text>
+                <View style={styles.modalButtons}>
+                    <Pressable
+                        style={[styles.button, styles.buttonCancel]}
+                        onPress={() => setModalVisible(!modalVisible)}>
+                        <Text style={styles.textStyle}>Cancel</Text>
+                    </Pressable>
+                    <View style={{ marginHorizontal: 25 }} />
+                    <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => setModalVisible(!modalVisible)}>
+                        <Text style={styles.textStyle}>Create</Text>
+                    </Pressable>
+                </View>
+              </View>
             </View>
-            <View style={styles.InnerContainer2}>
-            </View>
-            <View style={styles.InnerContainer3}>
-                <IconButton icon={iconName} size={50} iconColor={Colors.primary} onPress={onPressCreateEvent}/>
-            </View>
+          </Modal>
+          <Pressable
+            style={[styles.button, styles.buttonOpen]}
+            onPress={() => setModalVisible(true)}>
+            <Text style={styles.textStyle}>Add</Text>
+          </Pressable>
         </View>
-    )
-}
-
+      );
+    };
+    
 const styles = StyleSheet.create({
-    OutMostContainer: {
+    centeredView: {
         flex: 1,
-        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+        marginTop: 22,
+        },
+        modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
     },
-    InnerContainer1: {
-        flex:3,
-        backgroundColor: 'powderblue',
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        ...Platform.select({
+          ios: {
+            shadowColor: 'black',
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.3,
+            shadowRadius: 4,
+          },
+          android: {
+            elevation: 2,
+          },
+        }),
     },
-    InnerContainer2: {
-        flex:3,
-        backgroundColor: 'skyblue',
+    buttonOpen: {
+        backgroundColor: '#2196F3',
     },
-    InnerContainer3: {
-        flex:1,
-        backgroundColor: 'steelblue',
+    buttonClose: {
+        backgroundColor: '#2196F3',
     },
-})
+    buttonCancel: {
+        backgroundColor: 'grey',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 25,
+        textAlign: 'center',
+    },
+    modalButtons: {
+        flexDirection: 'row', // Puts the components horizontally
+        justifyContent: 'center', // Aligns the components horizontally in the center
+    }
+});
 
 export default EventsPage;
